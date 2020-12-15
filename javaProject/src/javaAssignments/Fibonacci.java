7984d2eaa71592bd294394f38ab4499641093dd0
Moise Jerome <moisejerome@Moises-Air.attlocal.net>
2020-12-15 14:20:56 -0500

    Finished 3 projects of day

--- javaProject/src/javaAssignments/Fibonacci.java
+++ javaProject/src/javaAssignments/Fibonacci.java
@@ -1,5 +1,36 @@
 package javaAssignments;
 
-public class Fibonacci {
+import java.util.Scanner;
 
+public class Fibonacci {
+	
+private static Scanner myScanner;
+
+public static void main(String[] args) {
+
+	myScanner = new Scanner(System.in);
+	System.out.println("We are going to what Fib. number?: ");
+	int x = myScanner.nextInt();
+	
+	numFibonacci(x);
+}
+
+public static void numFibonacci(int number) {
+	
+	int a = 1;
+	int b = 1;
+	System.out.print("0 ");
+	System.out.print(a + " ");
+	System.out.print(b + " ");
+	
+	for(int i = 0; i < number; i++){
+		int c = a+b;
+		a = b;
+		b = c;
+		System.out.print(c+ " ");
+		
+	}
+	
+	
+}
 }
